00be756d0ba0f82a21621be337243ced3056fdca
Dimitri Bolotnikov <148213666+DitoGeorgia@users.noreply.github.com>
2024-07-25 15:15:31 +0400

    Business card New

--- Lesson_HW_1.3/src/Main2.java
+++ Lesson_HW_1.3/src/Main2.java
@@ -1,7 +1,7 @@
 public class Main2 {
     public static void main(String[] args) {
         System.out.print("*****************************\n");
-        System.out.print("****       Визитка       ****\n");
+        System.out.print("****    Business card    ****\n");
         System.out.print("****  Name: \"Dimitri\"    ****\n");
         System.out.print("**** Skype: ditogeorgia  ****\n");
         System.out.print("*****************************\n");
