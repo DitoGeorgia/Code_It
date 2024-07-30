4c8c4fb600d1939affb94f44dabebf164efcca0a
DitoGeorgia <ditogeorgia@gmail.com>
2024-07-31 03:39:16 +0400

    Working with numbers

---
+++ Lesson_3/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        // Комментарий
+
+        /*
+        Hello World
+         */
+
+        /**
+         * @Javadoc
+         */
+
+        System.out.println(10+3); // 13
+        System.out.println(10-3); // 7
+        System.out.println(10*3); // 30
+        System.out.println(10/3); // 3
+        System.out.println(10.0/3);  // 3.3333333333333335
+        System.out.println(10%3); //1
+        System.out.println(3+5*2); // 13
+        System.out.println((3+5)*2); // 16
+
+
+        System.out.println("3" + 1); // 31 строка + число = строка
+                                     // В Java нельзя строку " - " ; " * " ; " / "
+
+        
+    }
+}
