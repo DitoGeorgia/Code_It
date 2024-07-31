3b36bfa56e71f74f328a6b094d3d8443a99328e9
DitoGeorgia <ditogeorgia@gmail.com>
2024-07-31 04:48:09 +0400

    Celsius to Fahrenheit

---
+++ Lesson_HW_3.1/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // F = C * 1.8 + 32
+
+        System.out.println("Температура по цельсию: " + 32);
+        System.out.println(32 + "C" + " = " + (32 * 1.8 + 32) + "F" );
+
+
+
+        System.out.println("Температура по цельсию: " + 32);
+
+        int C = 32;
+        double F = C * 1.8 + 32;
+        System.out.println(32 + "C" + " = " + F + "F" );
+
+    }
+}
