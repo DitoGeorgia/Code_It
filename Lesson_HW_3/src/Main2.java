f2de0cf3edf1d5fc431b46defcf555409c64e069
DitoGeorgia <ditogeorgia@gmail.com>
2024-07-31 07:05:36 +0400

    Calculator

---
+++ Lesson_HW_3/src/Main2.java
@@ -0,0 +1,10 @@
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Введите сумму в ₾ : " + 100);
+        System.out.println(50 + "₾" + " = " + 100 * 0.37 + "$");
+        System.out.println(50 + "₾" + " = " + 100 * 0.34 + "€\n");
+
+        System.out.println(50 + "₾" + " = " + 100 * (1/2.71) + "$");
+        System.out.println(50 + "₾" + " = " + 100 * (1/2.94) + "€");
+    }
+}
