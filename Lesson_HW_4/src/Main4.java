a98995b9405d798925b296337177ff947aba43d0
DitoGeorgia <ditogeorgia@gmail.com>
2024-08-03 15:17:02 +0400

    Calculator $/€

--- Lesson_HW_4/src/Main4.java
+++ Lesson_HW_4/src/Main4.java
@@ -7,11 +7,15 @@ public class Main4 {
 
         System.out.println("Введите сумму в ₾ : ");
         double summ = new Scanner(System.in).nextDouble();
+        System.out.println("Введите курс \"$\" : ");
+        double dollarPrice = new Scanner(System.in).nextDouble();
+        System.out.println("Введите курс \"€\" : ");
+        double euroPrice = new Scanner(System.in).nextDouble();
 
         System.out.println(summ + "₾" + " = " + summ * 0.37 + " $");
         System.out.println(summ + "₾" + " = " + summ * 0.34 + " €\n");
 
-        System.out.println(summ + "₾" + " = " + summ * (1/2.71) + " $");
-        System.out.println(summ + "₾" + " = " + summ * (1/2.94) + " €");
+        System.out.println(summ + "₾" + " = " + summ * (1/dollarPrice) + " $");
+        System.out.println(summ + "₾" + " = " + summ * (1/euroPrice) + " €");
     }
 }
