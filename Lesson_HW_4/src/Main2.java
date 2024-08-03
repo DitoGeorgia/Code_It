8f5a2743740c9e78b4f338f2d05991bad9cad014
DitoGeorgia <ditogeorgia@gmail.com>
2024-08-03 14:15:42 +0400

    Calculator

---
+++ Lesson_HW_4/src/Main2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("\n\t\t***Калькулятор!!!***\n");
+
+        System.out.println("Введите первое число: ");
+        double a = new Scanner(System.in).nextDouble();
+        System.out.println("Введите второе число: ");
+        double b = new Scanner(System.in).nextDouble();
+
+        System.out.println(a + " + " + b + " = " + (a + b) );
+        System.out.println(a + " - " + b + " = " + (a - b) );
+        System.out.println(a + " * " + b + " = " + (a * b) );
+        System.out.println(a + " / " + b + " = " + (a / b) );
+    }
+}
