f7fe99e96275b951fa646738dff7a5d1cd6c0473
DitoGeorgia <ditogeorgia@gmail.com>
2024-08-03 14:54:21 +0400

    Calculator $/€

--- Lesson_HW_4/src/Main4.java
+++ Lesson_HW_4/src/Main4.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 
 public class Main4 {
     public static void main(String[] args) {
+
+        System.out.println("\n\t\t***Конвертер валют***\n");
+
         System.out.println("Введите сумму в ₾ : ");
         double summ = new Scanner(System.in).nextDouble();
+
         System.out.println(summ + "₾" + " = " + summ * 0.37 + " $");
         System.out.println(summ + "₾" + " = " + summ * 0.34 + " €\n");
 
