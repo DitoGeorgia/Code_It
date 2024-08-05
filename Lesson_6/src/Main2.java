0214f382f625f27e0cd8994701e8d486c71bb389
DitoGeorgia <ditogeorgia@gmail.com>
2024-08-05 06:19:36 +0400

    Unrelated conditions

---
+++ Lesson_6/src/Main2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Первая: ");
+        int a = new Scanner(System.in).nextInt();
+        if (a == 5) {
+            System.out.println("5");
+        }
+        a = 6;
+        if (a == 6) {
+            System.out.println("6");
+        }
+        a = 7;
+        if (a == 7) {
+            System.out.println("7");
+        }
+    }
+}
