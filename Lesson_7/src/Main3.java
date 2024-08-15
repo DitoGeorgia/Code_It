defcd3c5dfeb5989eb8348113a7bd19dcef34921
Dimitri Bolotnikov <148213666+DitoGeorgia@users.noreply.github.com>
2024-08-16 02:00:23 +0400

    do

---
+++ Lesson_7/src/Main3.java
@@ -0,0 +1,15 @@
+public class Main3 {
+    public static void main(String[] args) {
+        boolean isCantinue = false;
+        do {
+            System.out.println("Hello");
+        } while (isCantinue);
+
+
+//        boolean isCantinue = false;
+//        while (isCantinue) {
+//            System.out.println("Hello");
+//        }
+
+    }
+}
