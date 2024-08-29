604dd8b19ac92057644fbda90db2100944ce2554
DitoGeorgia <ditogeorgia@gmail.com>
2024-08-30 00:19:57 +0400

    HW_9.1

--- Code_It_HW_9.1/src/Main.java
+++ Code_It_HW_9.1/src/Main.java
@@ -1,36 +1,42 @@
+
+//     В задании вам будет нужно выполнить все задачи из комментариев.
+
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
 
     public static void main(String[] args) {
-        //todo Вам нужно дополнить методы так, чтобы получился рабочий калькулятор
-        a = inputDouble();
-        b = inputDouble();
+        while (true) {
+            a = inputDouble();
+            b = inputDouble();
 
-        sum();      //Метод суммирования чисел
-        multiply(); //Метод умножения чисел
-        diff();     //Метод вычитания чисел
-        divide();   //Метод деления чисел
+            sum();      //Метод суммирования чисел
+            multiply(); //Метод умножения чисел
+            diff();     //Метод вычитания чисел
+            divide();   //Метод деления чисел
+        }
     }
 
     private static void divide() {
-        //todo дописать логику и печать результата
+        System.out.println("Деление: " + a + " / " + b + " = " + (a - b));
     }
 
     private static void diff() {
-        //todo дописать логику и печать результата
+        System.out.println("Вычитание: " + a + " - " + b + " = " + (a - b));
     }
 
     private static void multiply() {
-        //todo дописать логику и печать результата
+        System.out.println("Умножение: " + a + " * " + b + " = " + (a * b));
     }
 
     private static void sum() {
-        //todo дописать логику и печать результата
+        System.out.println("Сумма: " + a + " + " + b + " = " + (a + b));
     }
 
     private static double inputDouble() {
-        //todo дописать логику
-        return 0;
+        System.out.println("Введите число:");
+        return new Scanner(System.in).nextDouble();
     }
 }
