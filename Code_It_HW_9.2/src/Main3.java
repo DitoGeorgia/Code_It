5f5c4ca82b2d70909256e4c0d3e8b9e5bd56c36a
DitoGeorgia <ditogeorgia@gmail.com>
2024-08-30 03:12:46 +0400

    add3

--- Code_It_HW_9.2/src/Main3.java
+++ Code_It_HW_9.2/src/Main3.java
@@ -4,6 +4,7 @@ public class Main3 {
     public static void main(String[] args) {
         System.out.println("Введите число:");
         int input = new Scanner(System.in).nextInt();
-        System.out.println(input % 2 == 0 ? true : false);
+//        System.out.println(input % 2 == 0 ? true : false);
+        System.out.println(input % 2 == 0);
     }
 }
