3cf94442f510a1d30f5152a6c9674bef7a89e138
DitoGeorgia <ditogeorgia@gmail.com>
2024-12-03 18:55:14 +0400

    Конвертер Gigabyte to Megabyte / Калькулятор BMI

---
+++ Lesson_3.1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\t\t\tКонвертер Gigabyte to Megabyte");
+
+        System.out.println("В " + 4 + " Gigabyte: " + (4 * 1024) + " Megabyte");
+        System.out.println("В " + 8 + " Gigabyte: " + (8 * 1024) + " Megabyte");
+        System.out.println("В " + 16 + " Gigabyte: " + (16 * 1024) + " Megabyte");
+        System.out.println("В " + 32 + " Gigabyte: " + (32 * 1024) + " Megabyte");
+    }
+}
---
+++ Lesson_3.1/src/Main2.java
@@ -0,0 +1,9 @@
+//    Индекс массы тела (Body mass index)
+//    Индекс Массы Тела рассчитывается по формуле: ИМТ= вес (кг) / рост (м)2.
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("\t***Калькулятор BMI***");
+        System.out.println("BMI: " + 75.0 / (1.8 * 1.8));
+    }
+}
