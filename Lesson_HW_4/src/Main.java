a5a733fcb7ebf12be33297235742dd992d5ff624
DitoGeorgia <ditogeorgia@gmail.com>
2024-12-09 20:01:40 +0400

    add

---
+++ Lesson_HW_4/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int catCount = 45;
+        int testVar = 11;
+        int someVarName = 56;
+
+        String s = "some text";
+        char c = 'c';
+        char d = 'd';
+
+        int test = 10;
+        double f = 3.2;
+
+        System.out.println(a + "\n" + b + "\n" + catCount + "\n" + testVar + "\n" +
+                someVarName + "\n" + s + "\n" + c + "\n" + d + "\n" + test + "\n" + f);
+    }
+}
